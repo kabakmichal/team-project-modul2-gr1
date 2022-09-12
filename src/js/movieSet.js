ec2572e715b1a21cda539913b4622e88892c4dd4
KacperPasnicki <kacper.pas@hotmail.com>
2022-09-12 22:29:18 +0200

    .
    przy slice na dacie występował błąd podczas ponownego wyszukiwania. slice usunąłęm.
    nie działa zawijanie tekstu

--- src/js/movieSet.js
+++ src/js/movieSet.js
@@ -6,10 +6,9 @@ let movieSet = async data => {
   const markup = data
     .map(
       //trzeba dodać genre, ale mamy tylko genre_ids(cyfry)
-      ({ poster_path, release_date,first_air_date, title, name, genre_ids }) => {
+      ({ poster_path, release_date, first_air_date, title, name, genre_ids }) => {
         return `<div class="movie-card">
-    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${
-          title || name}" loading="lazy" />
+    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" onerror="this.src = 'https://picsum.photos/id/237/274/398';alt="image of movie" loading="lazy" />
     <div class="movie-card__info">
         <p class="movie-card__title">
             <span>${title || name}</span>
@@ -18,9 +17,8 @@ let movieSet = async data => {
         <p class="movie-card__genre">
             <span>${genre_ids.map(id => genreDict[id]).join(', ')}</span>
         </p>
-        <p class="movie-card__year">${(
-          release_date ||
-          first_air_date).slice(0, 4)}
+        <p class="movie-card__year">${
+          release_date || first_air_date}
             <span></span>
         </p>
     </div>
