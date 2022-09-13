b2dc77218833baf4e5641c4dcd27aa738f289aa1
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2022-09-13 21:15:13 +0200

    kot do pustych wyników

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -3,9 +3,13 @@ import {paginationBtns } from './fetch.js'
 
 const spinner = document.querySelector(".input-notification");
 
+const noResults = document.querySelector(".no-results__wrapper")
+
 export function spinnerHidden() {
 spinner.style.visibility = 'hidden';
 spinner.style.display = 'none';
+noResults.style.visibility = 'hidden';
+noResults.style.display = 'none';
 paginationBtns.style.visibility = "visible";
 paginationBtns.style.display = "flex";
 }
@@ -14,5 +18,8 @@ export function spinnerVisible() {
 spinner.style.visibility = 'visible';
 spinner.style.display = 'block';
 paginationBtns.style.visibility = "hidden";
-gallery.innerHTML = '';
+noResults.style.visibility = 'visible';
+noResults.style.display = 'block';  
+gallery.innerHTML = "";
 }
+
