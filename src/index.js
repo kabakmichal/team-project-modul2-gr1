4076b9623084b1e8ee92dad12d5ac949ea918b80
paliszoman <kamilkrolik@hotmail.com>
2022-09-14 02:26:21 +0200

    new

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import './sass/index.scss';
 
 import { fetchOnStart } from './js/fetch';
 import { movieSet } from './js/movieSet';
-
+let indexOfFilm;
+let movieInfo;
 fetchOnStart()
   .then(data => {
-    return movieSet(data.results);
+    return movieSet(data.results),
+    data.results;
   })
+  .then(data => {
+    document.querySelector(".gallery").addEventListener('click', (event) => {
+      indexOfFilm = data.findIndex(film => film.id == event.target.id),
+      movieInfo = data[indexOfFilm],
+      console.log(movieInfo)
+    }
+    )
+  
+})
   .catch(err => {
     return console.error(err);
   });
--- src/js/movieSet.js
+++ src/js/movieSet.js
@@ -6,9 +6,9 @@ let movieSet = async data => {
   const markup = data
     .map(
       //trzeba dodać genre, ale mamy tylko genre_ids(cyfry)
-      ({ poster_path, release_date, first_air_date, title, name, genre_ids }) => {
+      ({ poster_path, release_date, first_air_date, title, name, genre_ids, id }) => {
         return `<div class="movie-card">
-    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" onerror="this.src = 'https://picsum.photos/id/237/274/398';alt="image of movie" loading="lazy" />
+    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" onerror="this.src = 'https://picsum.photos/id/237/274/398';alt="image of movie" loading="lazy" id='${id}' />
     <div class="movie-card__info">
         <p class="movie-card__title">
             <span>${title || name}</span>
