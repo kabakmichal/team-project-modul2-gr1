93ed03ae44e16b943a94f5b5610ef1d0ec66f796
Michał Jarek <miciu77@gmail.com>
2022-09-15 16:39:21 +0200

    Movie search added

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,14 @@
+const fetchOneMovie = key => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${key}?api_key=32592fc1c467ab313147df8555d6672d`
+  ).then(response => {
+    if (!response.ok) throw new Error(response.status);
+    return response.json();
+  });
+
+  return movie;
+};
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
@@ -5,11 +16,13 @@
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', event => {
+  refs.openModalBtn.addEventListener('click', async event => {
     if (event.target.offsetParent.className !== 'movie-card') return;
     toggleModal();
     let movieId = event.target.offsetParent.dataset.id;
     console.log(movieId);
+    let movie = fetchOneMovie(movieId);
+    console.log(movie);
   });
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
