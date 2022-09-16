b71afd59f8b281f80fc5372e89aba7bf92193d65
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-09-16 18:38:45 +0200

    Update movieSet.js

--- src/js/movieSet.js
+++ src/js/movieSet.js
@@ -10,7 +10,7 @@ let movieSet = async data => {
       ({ id, media_type, poster_path, release_date, first_air_date, title, name, genre_ids }) => {
         let filmDate = release_date || first_air_date || '????';
         return `<div class="movie-card" data-id="${id}" data-type="${media_type}">
-    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" onerror="this.src = 'https://picsum.photos/id/237/274/398';alt="image of movie" loading="lazy" />
+    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" onerror="this.src = 'https://picsum.photos/id/237/274/398'";alt="image of movie" loading="lazy" />
     <div class="movie-card__info">
         <p class="movie-card__title">
             <span>${title || name}</span>
