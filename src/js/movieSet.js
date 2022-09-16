504e46354fe2d08b1968fcdedce83c90fe03eb1c
paliszoman <kamilkrolik@hotmail.com>
2022-09-16 11:15:07 +0200

    Update movieSet.js

--- src/js/movieSet.js
+++ src/js/movieSet.js
@@ -3,13 +3,15 @@ const gallery = document.querySelector('.gallery');
 
 let movieSet = async data => {
   const genreDict = await getGenre();
+  gallery.innerHTML = "";
   const markup = data
     .map(
       //trzeba dodać genre, ale mamy tylko genre_ids(cyfry)
       ({ id, poster_path, release_date, first_air_date, title, name, genre_ids }) => {
         let filmDate = release_date || first_air_date;
+        if (filmDate === undefined) {return filmDate = null}
         return `<div class="movie-card" data-id="${id}">
-    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" onerror="this.src = 'https://picsum.photos/id/237/274/398';alt="image of movie" loading="lazy" />
+    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" onerror="this.src ='https://picsum.photos/id/237/274/398'" ;alt="image of movie" loading="lazy" />
     <div class="movie-card__info">
         <p class="movie-card__title">
             <span>${title || name}</span>
