d0943129818621af54720fad92b17e6006d77991
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-09-16 18:21:55 +0200

    Update modalSet.js

--- src/js/modalSet.js
+++ src/js/modalSet.js
@@ -1,58 +1,33 @@
-const movieModalBox = document.querySelector('.movie-box');
-
-let modalSet = async movie => {
-  movieModalBox.innerHTML = '';
-  console.log(movie);
-  const {
-    id,
-    imgAlt,
-    media_type,
-    poster_path,
-    release_date,
-    first_air_date,
-    title,
-    name,
-    genres,
-    vote_average,
-    vote_count,
-    popularity,
-    overview,
-  } = movie;
-  let markup = `<div class="movie-box-separation">
-                <div class="movie-box-photo"><img class="movie__image" src="#" alt="Poster of: ${imgAlt}"
-                        loading="lazy"></div>
-                <div class="movie-box-info">
-                    <h2 class="modal-movie-title">${title || name}</h2>
-                    <ul class="modal-movie-details">
-                        <li class="modal-movie-details-li"><span class=" details__label">Vote / Votes</span><span
-                                class="details__rating">${Number(
-                                  vote_average
-                                ).toFixed(1)}</span> /
-                            ${vote_count}</span></li>
-                        </li>
-                        <li class="modal-movie-details-li"><span class=" details__label">Popularity</span><span
-                                class="details__value">${Number(
-                                  popularity
-                                ).toFixed(0)}</span></li>
-                        <li class="modal-movie-details-li"><span class=" details__label">Original Title</span><span
-                                class="details__value details__title">${title}</span></li>
-                        <li class="modal-movie-details-li"><span class=" details__label">Genre</span><span
-                                class="details__value">${genres
-                                  .map(genre => genre.name)
-                                  .join(', ')}</span></li>
-                        </li>
-                    </ul>
-                    <h3 class="movie__label">About</h3>
-                    <p class="movie__about">${overview}</p>
-                    <div class="modal__btns--wrapper">
-                        <button type="button" class="modal__btns modal__btns--watch" data-name="watched"
-                            data-id=${id}>Add to watched</button>
-                        <button type="button" class="modal__btns modal__btns--queue" data-name="queue" data-id=${id}>Add
-                            to queue</button>
-                    </div>
-                </div>
-            </div>`;
-
-  return movieModalBox.insertAdjacentHTML('beforeend', markup);
+import { getGenre } from './getGenre';
+const gallery = document.querySelector('.gallery');
+let movieSet = async data => {
+  const genreDict = await getGenre();
+  gallery.innerHTML = "";
+  const markup = data
+    .map(
+      //trzeba dodać genre, ale mamy tylko genre_ids(cyfry)
+      ({ id, poster_path, release_date, first_air_date, title, name, genre_ids }) => {
+        let filmDate = release_date || first_air_date||"????";
+        return `<div class="movie-card" data-id="${id}">
+    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" onerror="this.src ='https://picsum.photos/id/237/274/398'" ;alt="image of movie" loading="lazy" />
+    <div class="movie-card__info">
+        <p class="movie-card__title">
+            <span>${title || name}</span>
+        </p>
+    <div class= "genreDate">
+        <p class="movie-card__genre">
+            <span>${genre_ids.map(id => genreDict[id]).splice(0,2).join(', ')}</span>
+        </p>
+        <p class="movie-card__year">| ${filmDate.slice(0,4)}
+            <span></span>
+        </p>
+    </div>
+    </div>
+</div>
+`;
+      }
+    )
+    .join('');
+  return gallery.insertAdjacentHTML('beforeend', markup);
 };
-export { modalSet };
+export { movieSet };
