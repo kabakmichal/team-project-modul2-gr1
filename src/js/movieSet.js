17d3171376f692866f39a84f3d777e4a6910265b
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-09-16 18:11:05 +0200

    Update movieSet.js

--- src/js/movieSet.js
+++ src/js/movieSet.js
@@ -1,42 +1,46 @@
-import { getGenre } from './getGenre';
-const gallery = document.querySelector('.gallery');
+import { modalSet } from './modalSet';
 
-let movieSet = async data => {
-  const genreDict = await getGenre();
-  gallery.innerHTML = "";
-  const markup = data
-    .map(
-      //trzeba dodać genre, ale mamy tylko genre_ids(cyfry)
-
-      ({ id, poster_path, release_date, first_air_date, title, name, genre_ids }) => {
-        let filmDate = release_date || first_air_date||"????";
-        return `<div class="movie-card" data-id="${id}">
-    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" onerror="this.src ='https://picsum.photos/id/237/274/398'" ;alt="image of movie" loading="lazy" />
-
-    <div class="movie-card__info">
-        <p class="movie-card__title">
-            <span>${title || name}</span>
-
-        </p>
-
-    <div class= "genreDate">
-        <p class="movie-card__genre">
-            <span>${genre_ids.map(id => genreDict[id]).splice(0,2).join(', ')}</span>
-
-        </p>
-
-        <p class="movie-card__year">| ${filmDate.slice(0,4)}
-            <span></span>
-
-        </p>
-    </div>
-    </div>
-</div>
-`;
-       
-      }
-    )
-    .join('');
-  return gallery.insertAdjacentHTML('beforeend', markup);
+const fetchOneMovie = key => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${key}?api_key=32592fc1c467ab313147df8555d6672d`
+  ).then(response => {
+    if (!response.ok) throw new Error(response.status);
+    return response.json();
+  });
 };
-export { movieSet };
+const fetchOneTV = key => {
+  return fetch(
+    `https://api.themoviedb.org/3/tv/${key}?api_key=32592fc1c467ab313147df8555d6672d`
+  ).then(response => {
+    if (!response.ok) throw new Error(response.status);
+    return response.json();
+  });
+};
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  // Zmienna movie przechowuje obiekt z konkretnym filmem !!!!!!!
+
+  refs.openModalBtn.addEventListener('click', async event => {
+    if (event.target.offsetParent.className !== 'movie-card') return;
+    toggleModal();
+    let movieId = event.target.offsetParent.dataset.id;
+    let movie;
+    // console.log(event.target.offsetParent.dataset.type);
+    if (event.target.offsetParent.dataset.type === 'movie')
+      movie = await fetchOneMovie(movieId);
+    if (event.target.offsetParent.dataset.type === 'tv')
+      movie = await fetchOneTV(movieId);
+    modalSet(movie);
+  });
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
