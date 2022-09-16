67cefc08856dde2912a13eaf6a3c080d14f92480
paliszoman <kamilkrolik@hotmail.com>
2022-09-16 11:19:09 +0200

    data poprawiona

--- src/js/movieSet.js
+++ src/js/movieSet.js
@@ -9,11 +9,7 @@ let movieSet = async data => {
       //trzeba dodać genre, ale mamy tylko genre_ids(cyfry)
       ({ id, poster_path, release_date, first_air_date, title, name, genre_ids }) => {
         let filmDate = release_date || first_air_date;
-<<<<<<< Updated upstream
-        if (filmDate === undefined) {return filmDate = null}
-=======
-        if (filmDate === undefined) {return filmDate = null }
->>>>>>> Stashed changes
+        if (filmDate === undefined) { return filmDate = null }
         return `<div class="movie-card" data-id="${id}">
     <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" onerror="this.src ='https://picsum.photos/id/237/274/398'" ;alt="image of movie" loading="lazy" />
     <div class="movie-card__info">
