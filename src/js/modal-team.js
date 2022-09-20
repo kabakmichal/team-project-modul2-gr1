6312ccb6eda770a2d0243c8589f52219a8c0a89c
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2022-09-20 14:58:52 +0200

    style form

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -8,8 +8,20 @@
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 
+document.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+    refs.modal.classList.add('is-hidden');
+  }
+});
+  
+//   document.addEventListener('click', event => {
+//   if (event.target.matches('[data-modal-team-close]') || !event.target.closest('[data-modal-team]')) {
+//     refs.modal.classList.add('is-hidden');
+//   }
+// });
+  
+  
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
 })();
-
