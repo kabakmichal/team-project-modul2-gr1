ad8c091e5982412d3a734567fe89924ae5d89e38
Michał Jarek <miciu77@gmail.com>
2022-09-21 18:45:52 +0200

    first commit

--- src/js/libraryModal.js
+++ src/js/libraryModal.js
@@ -1,22 +1,7 @@
 import { modalSet, movieModalBox } from './modalSet';
 import { addToQueue, addToWatched } from './Queue';
+import { situation } from './library';
 
-const fetchOneMovie = key => {
-  return fetch(
-    `https://api.themoviedb.org/3/movie/${key}?api_key=32592fc1c467ab313147df8555d6672d`
-  ).then(response => {
-    if (!response.ok) throw new Error(response.status);
-    return response.json();
-  });
-};
-const fetchOneTV = key => {
-  return fetch(
-    `https://api.themoviedb.org/3/tv/${key}?api_key=32592fc1c467ab313147df8555d6672d`
-  ).then(response => {
-    if (!response.ok) throw new Error(response.status);
-    return response.json();
-  });
-};
 
 (() => {
   const refs = {
@@ -25,25 +10,17 @@ const fetchOneTV = key => {
     modal: document.querySelector('[data-modal]'),
   };
 
-  // Zmienna movie przechowuje obiekt z konkretnym filmem !!!!!!!
-  let counter = 0;
-  refs.openModalBtn.addEventListener('click', async event => {
+  refs.openModalBtn.addEventListener('click', event => {
     if (event.target.offsetParent.className !== 'movie-card') return;
     toggleModal();
     let movieId = event.target.offsetParent.dataset.id;
-    let movie;
-    console.log(event.target.offsetParent.dataset.type);
-    if (event.target.offsetParent.dataset.type === 'movie')
-      movie = await fetchOneMovie(movieId);
-    if (event.target.offsetParent.dataset.type === 'tv')
-      movie = await fetchOneTV(movieId);
-    await modalSet(movie);
+    let movie = JSON.parse(localStorage.getItem(situation))[movieId];
+    modalSet(movie);
     refs.modal.addEventListener('click', e => {
       if (e.target.dataset.name === 'queue') {
         addToQueue(movie);
         movie = '';
       }
-
       if (e.target.dataset.name === 'watched') {
         addToWatched(movie);
         movie = '';
@@ -63,3 +40,40 @@ const fetchOneTV = key => {
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const fetchOneMovie = key => {
+//   return fetch(
+//     `https://api.themoviedb.org/3/movie/${key}?api_key=32592fc1c467ab313147df8555d6672d`
+//   ).then(response => {
+//     if (!response.ok) throw new Error(response.status);
+//     return response.json();
+//   });
+// };
+// const fetchOneTV = key => {
+//   return fetch(
+//     `https://api.themoviedb.org/3/tv/${key}?api_key=32592fc1c467ab313147df8555d6672d`
+//   ).then(response => {
+//     if (!response.ok) throw new Error(response.status);
+//     return response.json();
+//   });
+// };
