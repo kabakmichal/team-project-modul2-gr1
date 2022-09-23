81f3c44a813367de917ea983d987670eab51f680
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2022-09-23 11:26:01 +0200

    scroll to top
    po załadowaniu kolejnej str filmów przerzuca nas na górę strony

--- src/js/scrollToTop.js
+++ src/js/scrollToTop.js
@@ -3,3 +3,8 @@
 export let topBtn = document.querySelector(".top-btn");
 topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
 window.onscroll = () => window.scrollY > 500 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0;
+
+toTop = ()=> window.scrollTo({top: 100,left: 100,behavior: 'smooth'
+});
+
+export { toTop };
