a2f4e9dbb914af9740d3cabeb756adabe2cc0481
Michał Jarek <miciu77@gmail.com>
2022-09-23 14:38:49 +0200

    podejście 2

--- src/js/scrollToTop.js
+++ src/js/scrollToTop.js
@@ -1,8 +1,11 @@
 //On scroll to TOP
 
-let topBtn = document.querySelector(".top-btn");
-topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
-window.onscroll = () => window.scrollY > 500 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0;
+let topBtn = document.querySelector('.top-btn');
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: 'smooth' });
+window.onscroll = () =>
+  window.scrollY > 500
+    ? (topBtn.style.opacity = 1)
+    : (topBtn.style.opacity = 0);
 
-toTop = ()=> window.scrollTo({top: 100,left: 100,behavior: 'smooth'
-});
+const toTop = () =>
+  window.scrollTo({ top: 100, left: 100, behavior: 'smooth' });
